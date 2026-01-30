f5df481fb72b0f32ad11bfbb415dce86507ac90e
RajendraPatil98 <patilrajendra5198@gmail.com>
2026-01-30 23:39:19 +0530

    Update: CORS configuration with frontend URL https://ghibli-frontend-for-stability-ai-ghibli.onrender.com

--- src/main/java/in/rajendrapatil/ghibliapi/controller/GenerationController.java
+++ src/main/java/in/rajendrapatil/ghibliapi/controller/GenerationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173", "https://ghibli-frontend-for-stability-ai-ghibli-app.onrender.com"})
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173", "https://ghibli-frontend-for-stability-ai-ghibli.onrender.com"})
 @RequiredArgsConstructor
 public class GenerationController {
 
